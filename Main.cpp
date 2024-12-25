5cead06446300e55c6aecad8d3ffb68974936a20
yinxunwen <77774299+yinxunwen@users.noreply.github.com>
2024-12-25 22:39:21 +0800

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <thread>
+#include <queue>//队列
+
+class GUI {
+    private:
+    public:
+};
 
 class main_module {
 private:
     bool first_run = true;
+    std::queue<int> Prophecy_notification_queue;
 public:
 
     struct data{//数据储存
@@ -14,14 +21,23 @@ public:
     {
         int position;
         bool type;//true为实弹，false为空包弹
+        bool Status_reminder = false;//状态提醒
     }Mobile;
     
 
-    void Probability_calculation(){//计算概率
-        int A = data.Number_of_bullets_Live_ammunition;
-        int B = data.Bullet_Quantity_Empty_Pack_Bullets;
-        std::cout<<"当前实弹概率为："<< A * 10 << "%" <<std::endl;
-        std::cout<<"当前空包弹概率为："<< B * 10 << "%" <<std::endl;
+    int Probability_calculation(){//计算概率 
+    /* 在一组奖品中，每个奖品有自己的概率，总概率为 1.0，也就是说在库存充足的情况下，必然能抽中其中的一个。
+    通过「谢谢参与」来作为无奖的奖品（也是一种奖品）需要注意的是：如果一组中所有的奖品，总概率之和不为 1.0，
+    那么数值代表的概率就不是真实概率了，需要用所占比例来作为新的概率：
+    新概率值=奖品概率/总概率。举个例子：只有 A 和 B 两个奖品，A 概率是 0.1，B 概率是 0.3，那么总概率就是 0.4，
+    A 的真实概率就是0.1/0.4=0.25，B 的真实概率是0.3/0.4=0.75，真实的总概率依然为1。
+    所以如果想要配置的奖品概率正好是抽奖时的概率值，那么就需要为这一组奖品列表的总概率配置成1.0 */
+        double Probability_of_live_ammunition = 0.1 * data.Number_of_bullets_Live_ammunition/*实弹概率*/ , Probability_of_empty_ammunition = 0.1 * data.Bullet_Quantity_Empty_Pack_Bullets/*空包弹概率*/ , Total_number_of_bullets = 0.1 * (data.Number_of_bullets_Live_ammunition + data.Bullet_Quantity_Empty_Pack_Bullets)/*总概率*/;
+        Probability_of_live_ammunition = 100 * (Probability_of_live_ammunition / Total_number_of_bullets);
+        Probability_of_empty_ammunition = 100 * (Probability_of_empty_ammunition / Total_number_of_bullets);
+        std::cout<<"实弹概率："<<Probability_of_live_ammunition<<"%"<<std::endl;
+        std::cout<<"空包弹概率："<<Probability_of_empty_ammunition<<"%"<<std::endl;
+        return 0;
     }
     std::pair<int, int> Input_output_processing(){//输入信息
         int A, B;
@@ -31,6 +47,10 @@ public:
             std::cin>> A;
             std::cout<<"请输入实弹数量："<<std::endl;
             std::cin>> B;
+            if (A == 10)
+            {
+                std::cout<<"认真的？"<<std::endl;
+            }
             if (A + B <= 10)
             {
                 return std::make_pair(A, B);
@@ -54,6 +74,7 @@ public:
         }else if (BO == 3){
             return -1;
         }
+        Prophecy_notification_queue.push(1);
         return 0;
     }
 
@@ -73,12 +94,12 @@ public:
         }
         return 0;
     }
-
     int Mobile_prophecy_record(){//手机预言记录模式
         int position;
         bool type;
         while (true)
         {
+            system("cls");
             std::cout<<"手机预言记录模式"<<std::endl;
             std::cout<<"请输入子弹位置'第多少发'"<<std::endl;
             std::cin>> position;
@@ -106,18 +127,19 @@ public:
     }
 
     void count_down(){//手机预言计时器
-    for (int A = Mobile.position - 1;;)
-    {
-        if (Mobile.position == 0)
+        while (true)
         {
-            if (Mobile.type)
+            if (Prophecy_notification_queue.size() >= 1)
             {
-                std::cout<<"手机预言提醒这一发为实弹"<<std::endl;
-            } else {
-                std::cout<<"手机预言提醒这一发为空包弹"<<std::endl;
+                Prophecy_notification_queue.pop();
+                Mobile.position--;
+                if (Mobile.position == 0)
+                {
+                    Mobile.Status_reminder = false;
+                    break;
+                }
             }
         }
-    }
     }
     int Start_and_initialization(){//开始和初始化
         if (first_run)
@@ -143,18 +165,27 @@ int main(){//主函数
     while (true)
     {
         //system("cls");
-        //mod.Probability_calculation();
-        std::cout<<"请输入'1'或者'2'输入'3'转换下一发子弹"<<std::endl;/*<<"输入'4'进入手机预言模式"<<std::endl*/;
+        mod.Probability_calculation();
+        if (mod.Mobile.Status_reminder)//true为提醒
+        {
+            if (mod.Mobile.type)
+            {
+                std::cout<<"手机预言提醒这一发为实弹"<<std::endl;
+            } else {
+                std::cout<<"手机预言提醒这一发为空包弹"<<std::endl;
+            }
+        }
+        std::cout<<"请输入'1'或者'2'输入'3'转换下一发子弹"<<std::endl<<"输入'4'进入手机预言模式"<<std::endl;
         std::cin>> input;
         if (input == 1 || input == 2)
         {
             mod.Calculation_of_bullet_quantity(input);
         }else if (input == 3){
             mod.Converter_conversion_compatibility();
-        /*}else if (input == 4){
+        }else if (input == 4){
             mod.Mobile_prophecy_record();
             std::thread t1(&main_module::count_down,&mod);
-            t1.detach();*/
+            t1.detach();
         }
         if (mod.data.Number_of_bullets_Live_ammunition == 0 && mod.data.Bullet_Quantity_Empty_Pack_Bullets == 0)
         {
